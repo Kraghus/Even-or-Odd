d8b1206f919a6d5c1c004e88960321436cc97b5c
Kraghus <masongage@outlook.com>
2022-05-22 11:56:02 -0400

    Working program
    Program functions as intended.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+int getInteger()
+{
+	std::cout << "Enter an iteger: ";
+	int num{};
+	std::cin >> num;
+	return num;
+}
+
+bool isEven(int num)
+{
+	return ((num % 2) == 0);
+}
+
 int main()
 {
+	int num{ getInteger() };
+
+	if (isEven(num))
+		std::cout << num << " is even.\n";
+	else
+		std::cout << num << " is odd.\n";
+
 	return 0;
 }
